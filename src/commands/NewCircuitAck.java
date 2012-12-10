684f3eb9c715eb0e33b319d36d00d349a3583960
Joshua Rogers <joshuarogers10@gmail.com>
2012-12-10 16:03:06 -0500

    added files this time

---
+++ src/commands/NewCircuitAck.java
@@ -0,0 +1,14 @@
+package commands;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+
+public class NewCircuitAck extends ORCommand{
+
+	@Override
+	protected void ORDecode(ChannelHandlerContext ctx, Channel ch, ChannelBuffer buffer){
+		
+	}
+	
+}
